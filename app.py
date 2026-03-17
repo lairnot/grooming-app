dfb8d50ddecebf4e734983d8e7aab263acde50f4
lairnot <lairnot@hotmail.com>
2026-03-17 07:14:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -351,8 +351,8 @@ def services():
 def inject_now():
     return {"now": datetime.now()}
 
+with app.app_context():
+    init_db()
 
-if __name__ == "__main__":
-    with app.app_context():
-        init_db()
+if _name_ == "_main_":
     app.run(debug=True)
